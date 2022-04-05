530b150fc541e563b7f6df5cb247d97e5ad1c799
QuimeyRodi <quimeyrodi@gmail.com>
2022-04-05 18:12:23 -0300

    Se agrego la precision de la fecha a la base de datos

--- app/src/main/java/ayds/winchester/songinfo/home/model/repository/local/spotify/sqldb/SpotifyTableConstants.kt
+++ app/src/main/java/ayds/winchester/songinfo/home/model/repository/local/spotify/sqldb/SpotifyTableConstants.kt
@@ -9,6 +9,7 @@ const val ALBUM_COLUMN = "album"
 const val RELEASE_DATE_COLUMN = "release_date"
 const val SPOTIFY_URL_COLUMN = "spotify_url"
 const val IMAGE_URL_COLUMN = "image_url"
+const val PRECISION_DATE_COLUMN = "precision_date"
 
 const val createSongsTableQuery: String =
     "create table $SONGS_TABLE (" +
@@ -20,3 +21,6 @@ const val createSongsTableQuery: String =
             "$RELEASE_DATE_COLUMN string, " +
             "$SPOTIFY_URL_COLUMN string, " +
             "$IMAGE_URL_COLUMN string)"
+
+const val addReleaseDataQuery: String =
+    "ALTER TABLE $SONGS_TABLE ADD COLUMN $PRECISION_DATE_COLUMN string"
