13bd3ebe48eb43e739e6cb6022889507d067edfb
Camila Arena <56278138+CamilaArena@users.noreply.github.com>
2022-04-21 18:44:23 -0300

    se reemplazaron Strings por Constantes

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
@@ -5,62 +5,65 @@ import android.database.sqlite.SQLiteDatabase
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
-import android.util.Log
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
-import java.sql.Statement
 import java.util.ArrayList
 
-class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", null, 1) {
+private const val ID = "id"
+private const val ARTIST = "artist"
+private const val ARTISTS = "artists"
+private const val INFO = "info"
+private const val SOURCE = "source"
+private const val SELECTION = "artist  = ?"
+private const val SORT_ORDER = "artist DESC"
+private const val DICTIONARY_DB = "dictionary.db"
+private const val DATABASE_VERSION = 1
+
+class DataBase(context: Context) : SQLiteOpenHelper(context, DICTIONARY_DB, null, DATABASE_VERSION) {
+    private val dataBaseColumns = arrayOf(
+        ID,
+        ARTIST,
+        INFO
+    )
+
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(
             "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)"
         )
-        Log.i("DB", "DB created")
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
-    fun saveArtist(artist: String?, info: String?) {
+    fun saveArtist(artist: String, info: String) {
         val dataBase = this.writableDatabase
         val values = createValueMap(artist,info)
-        val newRowId = dataBase.insert("artists", null, values)
+        dataBase.insert(ARTISTS, null, values)
     }
 
-    private fun createValueMap (artist: String?, info: String?): ContentValues {
+    private fun createValueMap (artist: String, info: String): ContentValues {
         val values = ContentValues()
-        values.put("artist", artist)
-        values.put("info", info)
-        values.put("source", 1)
+        values.put(ARTIST, artist)
+        values.put(INFO, info)
+        values.put(SOURCE, 1)
         return values
     }
 
-    fun getInfo(dbHelper: DataBase, artist: String): String? {
+    fun getInfo(dbHelper: DataBase, artist: String): String{
         val cursor = createCursor(dbHelper,artist)
-        var items = createItemsList(cursor)
+        val items = createItemsList(cursor)
         cursor.close()
-        return if (items.isEmpty()) null else items[0]
+        return items[0]
     }
 
     private fun createCursor (dbHelper: DataBase, artist: String) : Cursor{
         val dataBase = dbHelper.readableDatabase
-        val dataBaseColumns = arrayOf(
-            "id",
-            "artist",
-            "info"
-        )
-        val selection = "artist  = ?"
         val selectionArgs = arrayOf(artist)
-        val sortOrder = "artist DESC"
-        return dataBase.query("artists", dataBaseColumns, selection, selectionArgs, null, null, sortOrder)
+        return dataBase.query(ARTISTS, dataBaseColumns, SELECTION, selectionArgs, null, null, SORT_ORDER)
     }
 
     private fun createItemsList(cursor: Cursor): MutableList<String> {
         val items: MutableList<String> = ArrayList()
         while (cursor.moveToNext()) {
             val info = cursor.getString(
-                cursor.getColumnIndexOrThrow("info")
+                cursor.getColumnIndexOrThrow(INFO)
             )
             items.add(info)
         }
