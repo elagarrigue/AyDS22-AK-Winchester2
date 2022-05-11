eaef3170924ee03de0d16a0adc5a7bf717e30207
Tomas Felipe <tomasfelipe911@gmail.com>
2022-05-11 20:04:24 -0300

    Se completo ArtistDescriptionHelper

--- app/src/main/java/ayds/winchester/songinfo/moredetails/home/view/ArtistDescriptionHelper.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/home/view/ArtistDescriptionHelper.kt
@@ -1,12 +1,32 @@
 package ayds.winchester.songinfo.moredetails.home.view
 
 import ayds.winchester.songinfo.moredetails.home.model.entities.Description
+import java.lang.StringBuilder
+import java.util.*
 
 interface ArtistDescriptionHelper {
-    fun getTextArtistDescription(description: Description): String
+    fun getTextArtistDescription(description: Description, artistName: String): String
 }
+
+private const val PREFIX = "[*] "
+
 internal class ArtistDescriptionHelperImpl() : ArtistDescriptionHelper {
-    override fun getTextArtistDescription (description: Description) : String{
-        return ""
+
+    override fun getTextArtistDescription (description: Description, artistName: String) : String {
+        val descriptionText = if(description.isLocallyStored) PREFIX + description.description else description.description
+        return textToHtml(descriptionText, artistName)
     }
+}
+
+private fun textToHtml(descriptionText: String, artistName: String): String {
+    val builder = StringBuilder()
+    builder.append("<html><div width=400>")
+    builder.append("<font face=\"arial\">")
+    val textWithBold = descriptionText
+        .replace("'", " ")
+        .replace("\n", "<br>")
+        .replace("(?i)" + artistName.toRegex(), "<b>" + artistName.uppercase(Locale.getDefault()) + "</b>")
+    builder.append(textWithBold)
+    builder.append("</font></div></html>")
+    return builder.toString()
 }
