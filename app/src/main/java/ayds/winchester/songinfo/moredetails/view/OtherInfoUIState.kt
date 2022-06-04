c501a0fad3f038164e5c297fa7ba0f8fc1780e4e
QuimeyRodi <quimeyrodi@gmail.com>
2022-06-04 15:10:45 -0300

    correccion en la url del logo de nytimes

--- app/src/main/java/ayds/winchester/songinfo/moredetails/view/OtherInfoUIState.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/view/OtherInfoUIState.kt
@@ -12,7 +12,8 @@ data class OtherInfoUIState(
     companion object {
         const val URL_IMAGE_WIKIPEDIA = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
         const val URL_IMAGE_LASTFM = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
-        const val URL_IMAGE_TIMES = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/NYC_Montage_2014_4_-_Jleon.jpg/456px-NYC_Montage_2014_4_-_Jleon.jpg"
+        const val URL_IMAGE_TIMES = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
         const val URL_IMAGE_NOT_FOUND = "https://alfabetajuega.com/hero/2018/10/167687.alfabetajuega-problemas-tecnicos.jpg"
+
     }
 }
