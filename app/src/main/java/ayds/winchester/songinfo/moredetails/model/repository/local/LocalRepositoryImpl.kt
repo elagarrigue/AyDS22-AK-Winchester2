66a314c0300bf71476ef75f36c2be0f1a37fc2f2
Tomas Felipe <tomasfelipe911@gmail.com>
2022-06-08 16:53:14 -0300

    Se elimino función no utilizada. Se quito propiedad artistName del repositorio local.

--- app/src/main/java/ayds/winchester/songinfo/moredetails/model/repository/local/LocalRepositoryImpl.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/model/repository/local/LocalRepositoryImpl.kt
@@ -2,18 +2,15 @@ package ayds.winchester.songinfo.moredetails.model.repository.local
 
 import android.content.Context
 import ayds.winchester.songinfo.moredetails.model.entities.Card
-import ayds.winchester.songinfo.moredetails.model.entities.EmptyCard
 import ayds.winchester.songinfo.moredetails.model.repository.local.sqldb.DataBase
 import ayds.winchester.songinfo.moredetails.view.OtherInfoWindow
 
 interface LocalRepository {
-    fun updateArtistTerm(id : String)
     fun getCards(artistName:String): List<Card>
-    fun saveCards(cards : List<Card>)
+    fun saveCards(cards : List<Card>, artistName:String)
 }
 internal class LocalRepositoryImpl(private var otherInfoView : OtherInfoWindow) : LocalRepository{
     private lateinit var dataBase: DataBase
-    private lateinit var artistName: String
 
     init{
         initProperties()
@@ -23,16 +20,11 @@ internal class LocalRepositoryImpl(private var otherInfoView : OtherInfoWindow) 
         dataBase = DataBase(otherInfoView as Context)
     }
 
-    override fun updateArtistTerm(id : String){
-        dataBase.updateArtistTerm(artistName, id)
-    }
-
      override fun getCards(artistName:String) : List<Card>{
-         this.artistName = artistName
          return dataBase.getInfo(dataBase, artistName)
     }
 
-    override fun saveCards(cards : List<Card>){
+    override fun saveCards(cards : List<Card>, artistName:String){
         for(i in cards.indices) {
             dataBase.saveArtist(artistName, cards[i].description, cards[i].infoUrl, cards[i].source.ordinal, cards[i].sourceLogoUrl)
         }
