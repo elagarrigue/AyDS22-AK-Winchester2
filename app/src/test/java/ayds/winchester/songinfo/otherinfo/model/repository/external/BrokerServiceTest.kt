14fb6801fbf5973ef935c0866bb09e420d5bb651
Tomas Felipe <tomasfelipe911@gmail.com>
2022-06-08 17:11:43 -0300

    Se agregaron casos para cuando no todos los servicios devuelven el mismo resultado.

--- app/src/test/java/ayds/winchester/songinfo/otherinfo/model/repository/external/BrokerServiceTest.kt
+++ app/src/test/java/ayds/winchester/songinfo/otherinfo/model/repository/external/BrokerServiceTest.kt
@@ -27,7 +27,7 @@ class BrokerServiceTest {
         every{proxy1.getInfo("name")} returns card1
         every{proxy2.getInfo("name")} returns card2
         every{proxy3.getInfo("name")} returns card3
-        val result = broker.getInfo("name")
+        val result = broker.getCards("name")
 
         assertEquals(list,result)
     }
@@ -38,9 +38,105 @@ class BrokerServiceTest {
         every{proxy1.getInfo("name")} returns EmptyCard
         every{proxy2.getInfo("name")} returns EmptyCard
         every{proxy3.getInfo("name")} returns EmptyCard
-        val result = broker.getInfo("name")
+        val result = broker.getCards("name")
 
         assertEquals(list,result)
     }
+
+    @Test
+    fun `al menos un servicio retorna una card valida y al menos uno retorna una emptycard`() {
+        `primer card empty`()
+        `segunda card empty`()
+        `tercer card empty`()
+        `primer y segunda card empty`()
+        `primer y tercera card empty`()
+        `segunda y tercera card empty`()
+
+    }
+
+    fun `primer card empty`(){
+        val card1 : EmptyCard = mockk()
+        val card2 : CardDescription = mockk()
+        val card3 : CardDescription = mockk()
+        val list = arrayListOf(card1,card2,card3)
+
+        every{proxy1.getInfo("name")} returns card1
+        every{proxy2.getInfo("name")} returns card2
+        every{proxy3.getInfo("name")} returns card3
+        val result = broker.getCards("name")
+
+        assertEquals(list,result)
+    }
+
+    fun `segunda card empty`(){
+        val card1 : CardDescription = mockk()
+        val card2 : EmptyCard = mockk()
+        val card3 : CardDescription = mockk()
+        val list = arrayListOf(card1,card2,card3)
+
+        every{proxy1.getInfo("name")} returns card1
+        every{proxy2.getInfo("name")} returns card2
+        every{proxy3.getInfo("name")} returns card3
+        val result = broker.getCards("name")
+
+        assertEquals(list,result)
+    }
+
+    fun `tercer card empty`(){
+        val card1 : CardDescription = mockk()
+        val card2 : CardDescription = mockk()
+        val card3 : EmptyCard = mockk()
+        val list = arrayListOf(card1,card2,card3)
+
+        every{proxy1.getInfo("name")} returns card1
+        every{proxy2.getInfo("name")} returns card2
+        every{proxy3.getInfo("name")} returns card3
+        val result = broker.getCards("name")
+
+        assertEquals(list,result)
+    }
+
+    fun `primer y segunda card empty`(){
+        val card1 : EmptyCard = mockk()
+        val card2 : EmptyCard = mockk()
+        val card3 : CardDescription = mockk()
+        val list = arrayListOf(card1,card2,card3)
+
+        every{proxy1.getInfo("name")} returns card1
+        every{proxy2.getInfo("name")} returns card2
+        every{proxy3.getInfo("name")} returns card3
+        val result = broker.getCards("name")
+
+        assertEquals(list,result)
+    }
+
+    fun `primer y tercera card empty`(){
+        val card1 : EmptyCard = mockk()
+        val card2 : CardDescription = mockk()
+        val card3 : EmptyCard = mockk()
+        val list = arrayListOf(card1,card2,card3)
+
+        every{proxy1.getInfo("name")} returns card1
+        every{proxy2.getInfo("name")} returns card2
+        every{proxy3.getInfo("name")} returns card3
+        val result = broker.getCards("name")
+
+        assertEquals(list,result)
+    }
+
+    fun `segunda y tercera card empty`(){
+        val card1 : CardDescription = mockk()
+        val card2 : EmptyCard = mockk()
+        val card3 : EmptyCard = mockk()
+        val list = arrayListOf(card1,card2,card3)
+
+        every{proxy1.getInfo("name")} returns card1
+        every{proxy2.getInfo("name")} returns card2
+        every{proxy3.getInfo("name")} returns card3
+        val result = broker.getCards("name")
+
+        assertEquals(list,result)
+    }
+
 
 }
